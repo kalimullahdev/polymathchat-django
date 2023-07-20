7f87ac8b77a4266085172b579cd5bebe0cb8b746
Kalim Ullah <62144959+kalimullahdev@users.noreply.github.com>
2023-07-20 07:23:36 +0500

    minor fixing

--- polymathchat_django/settings.py
+++ polymathchat_django/settings.py
@@ -1,16 +1,12 @@
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
